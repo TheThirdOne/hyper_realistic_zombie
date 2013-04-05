3f6095409a09aeed8c973d949b96c183787a5e8f
Benjamin Landers <benjaminrlanders@gmail.com>
2013-04-05 15:14:28 -0700

    i missed even more

--- hyper_realistic_zombie-core/src/com/benjaminlanders/zombie/model/world/World.java
+++ hyper_realistic_zombie-core/src/com/benjaminlanders/zombie/model/world/World.java
@@ -1,8 +1,7 @@
 package com.benjaminlanders.zombie.model.world;
 /**
- * 
+ * The physical environment in the game. This includes the map, collision, 
  * @author Benjamin Landers
- *
  */
 public class World
 {
