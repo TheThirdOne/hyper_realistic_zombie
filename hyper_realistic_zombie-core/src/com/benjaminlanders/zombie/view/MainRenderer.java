bf0b9c2fe78b21f1731a3fc89ef285d56fc7ddd1
Benjamin Landers <benjaminrlanders@gmail.com>
2013-04-06 12:58:02 -0700

    improved rendrer

---
+++ hyper_realistic_zombie-core/src/com/benjaminlanders/zombie/view/MainRenderer.java
@@ -0,0 +1,21 @@
+package com.benjaminlanders.zombie.view;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.benjaminlanders.zombie.MainGame;
+
+public class MainRenderer extends Renderer
+{
+
+	public MainRenderer(SpriteBatch batch, MainGame reference)
+	{
+		super(batch, reference);
+	}
+
+	@Override
+	public void render(float delta) 
+	{
+		
+
+	}
+
+}
